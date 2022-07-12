8a1a48bf72fa6c544de9aa0ab3b764a8d0f50169
GunGalla <gungalla@gmail.com>
2022-07-12 22:05:39 +0300

    Add directory games and file even_game.py

---
+++ brain_games/games/even_game.py
@@ -0,0 +1,11 @@
+"""Logic module for brain-even game."""
+
+from asking_user import ask_is_even
+from welcome import welcome_user
+
+
+def brain_even_game():
+    """Start and play the game."""
+    welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    ask_is_even()
