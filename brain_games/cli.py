12da6e2ab0f1eaa157bc41574dcc50c2cf45b4e0
GunGalla <gungalla@gmail.com>
2022-07-21 17:01:20 +0300

    Fix cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,7 @@
 """Module for greeting user."""
-import prompt
 
 
 def welcome_user():
     """Ask user name and greet him."""
     name = prompt.string('May I have your name? ')
-    print('Hello, {0}!'.format(name))
+    print(f'Hello, {name}!')
