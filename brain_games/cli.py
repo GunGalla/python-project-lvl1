7bbbdedcc1ea03c566de471d5a8c8d29730819d1
GunGalla <gungalla@gmail.com>
2022-08-22 19:55:25 +0300

    Fix some grammar mistakes

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,7 +2,7 @@
 
 
 def welcome_user():
-    """Ask user name and greet him."""
+    """Ask username and greet him."""
     print('Welcome to the Brain Games!')
     name = input('May I have your name? ')
     print(f'Hello, {name}!')
